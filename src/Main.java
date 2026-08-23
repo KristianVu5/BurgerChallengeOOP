9f5347faf0bf9fb79f0b070e2a40685f0e45f0f6
Kristian Vu <krisivu5@gmail.com>
2026-08-23 15:17:59 +0300

    I created the project and made sample classes and their models and functions.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
 public class Main {
     static void main() {
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 1.50);
-        avocado.printItem();
+//        Burger burger = new Burger("regular",4.00);
+//        burger.addToppings("BACON","CHEESE","MAYO");
+//        burger.printItem();
+
+
+//        MealOrder mealOrder = new MealOrder();
+//        mealOrder.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        mealOrder.setDrinkSize("LARGE");
+//        mealOrder.printItemizedList();
+
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up","chili");
+//        secondMeal.addBurgerToppings("LETTUCE","CHEESE","MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE",
+                "CHEESE","MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+
     }
+
+
 }
